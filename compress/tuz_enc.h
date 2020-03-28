43291236530ca2729fee9be5334c0d6fffa1b013
sisong <sisong@gmail.com>
2020-03-28 21:32:05 +0800

    for comple in .C file

--- compress/tuz_enc.h
+++ compress/tuz_enc.h
@@ -27,6 +27,9 @@
 #ifndef _tuz_enc_h
 #define _tuz_enc_h
 #include "tuz_enc_types.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void tuz_defaultCompressProps(tuz_TCompressProps* out_props);
 
@@ -35,4 +38,7 @@ hpatch_StreamPos_t tuz_maxCompressedSize(hpatch_StreamPos_t data_size);
 hpatch_StreamPos_t tuz_compress(const hpatch_TStreamOutput* out_code,const hpatch_TStreamInput* data,
                                 const tuz_TCompressProps* props);
 
+#ifdef __cplusplus
+}
+#endif
 #endif //_tuz_enc_h
